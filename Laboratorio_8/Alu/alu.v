0075c9a6037bebc69d1cd64532c0afedf232c700
valeelorraine <68074596+valeelorraine@users.noreply.github.com>
2020-10-24 03:55:26 -0600

    Hoy si, final

--- Laboratorio_8/Alu/alu.v
+++ Laboratorio_8/Alu/alu.v
@@ -10,18 +10,18 @@ module ALU(input wire [3:0]D, Q,
             input wire [2:0]S,
             output reg [3:0]Y);
 
-    always @ (D, Q, S) begin
+    always @ (D, Q, S) begin //Registrar los cambios realizados en D o Q
       case (S)
-        3'b000: Y <= D & Q ; // Función: A AND B
-        3'b001: Y <= D | Q ; // Función: A OR B
-        3'b010: Y <= D + Q ; // Función: A ADD B
+        3'b000: Y <= D & Q ; // Función: D AND Q
+        3'b001: Y <= D | Q ; // Función: D OR Q
+        3'b010: Y <= D + Q ; // Función: D ADD Q
 
         3'b011: Y <= Y ; // Colocar el valor pervio
 
-        3'b100: Y <= D & (~Q) ; // Función A AND ~B
-        3'b101: Y <= D | (~Q) ; // Función A OR ~B
-        3'b110: Y <= D - Q ; // Función A LESS B
-        3'b111: Y <= (D < Q) ? 1:0 ; // Función STL. (Comparación entre A y B, por lo que si A > B = 0000, si A < B = 0001)
+        3'b100: Y <= D & (~Q) ; // Función D AND ~Q
+        3'b101: Y <= D | (~Q) ; // Función D OR ~Q
+        3'b110: Y <= D - Q ; // Función D LESS Q
+        3'b111: Y <= (D < Q) ? 1:0 ; // Función STL. (Comparación entre D y Q, por lo que si D > Q = 0000, si D < Q = 0001)
 
       default: Y <= 4'b0000 ;// Todo en 0
     endcase
--- Laboratorio_8/Alu/alu_tb.v
+++ Laboratorio_8/Alu/alu_tb.v
@@ -26,16 +26,16 @@ module testbench();
   $monitor("|%b  %b  %b | %b |", D, Q, S, Y);
 
 // Funciones a realizar
-  D = 4'b1010; Q = 4'b0011; S = 3'b000; //FUNCION  A AND B
-  #1 S = 3'b001; // Función A OR B
-  #1 S = 3'b010; // Función A ADD B
-  #1 S = 3'b010; // Función A ADD B
+  D = 4'b1010; Q = 4'b0011; S = 3'b000; //FUNCION  D AND Q
+  #1 S = 3'b001; // Función D OR Q
+  #1 S = 3'b010; // Función D ADD Q
+  #1 S = 3'b010; // Función D ADD Q
 
   #1 S = 3'b011; //Colocar el valor previo
 
-  #1 S = 3'b100; // Función A AND ~B
-  #1 S = 3'b101; // Función A OR ~B
-  #1 S = 3'b110; // Función A LESS B
+  #1 S = 3'b100; // Función D AND ~Q
+  #1 S = 3'b101; // Función D OR ~Q
+  #1 S = 3'b110; // Función D LESS Q
   #1 S = 3'b111; // Función STL
   #1 D = 4'b010; Q = 4'b0011; // Colocar todo en 0
 
